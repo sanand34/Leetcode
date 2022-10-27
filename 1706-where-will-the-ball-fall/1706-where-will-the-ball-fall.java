58c51c40ba0c841e1f40e8afd495d28d8246f68d
Sanchit Anand <sanchitanand34@gmail.com>
2022-10-28 00:31:15 +0530

    Time: 17 ms (14.65%), Space: 55.2 MB (5.79%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,29 +1,34 @@
 import java.util.*;
 class Solution {
     HashMap<Integer,HashMap<Integer,Integer>> hmp=new HashMap<>();
+    
     public int[] findBall(int[][] grid) {
-        
         
         int[] ans=new int[grid[0].length];
         
-        for(int i=0;i<grid[0].length;i++){
+        for(int i=0;i<grid[0].length;i++)
             ans[i]=Grid(grid,0,i);
-        }
+            
         return ans;
     }
     public int Grid(int[][] mat,int r,int c){
+  
        HashMap <Integer,Integer> temp=hmp.get(r);
-        if(temp!=null){
-            if(temp.get(c)!=null) return temp.get(c);
-        }
+        
+        if(temp!=null && temp.get(c)!=null) return temp.get(c);
+        
         if(r==mat.length) return c;
+        
         if(c+mat[r][c]<0 || c+mat[r][c]>=mat[0].length) return -1; 
+        
         if(mat[r][c]==mat[r][c+mat[r][c]]) {
+            
             HashMap <Integer,Integer> val=new HashMap<>();
             
             val.put(c,Grid(mat,r+1,c+mat[r][c]));
             hmp.put(r,val);
             return val.get(c);
+            
         }
             
         
